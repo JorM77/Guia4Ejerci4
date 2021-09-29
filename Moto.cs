8a7e004ebc4c98afe098fb5e62c055eb9419620c
Jorge Alberto Villatoro Merino <VM201812@alumno.udb.edu.sv>
2021-09-28 23:20:28 -0600

    Agregar archivos de proyecto.

---
+++ Moto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia4Ejerci4
+{
+    class Moto:Vehiculos
+    {
+        private bool _sidecar;
+
+        public Moto(int id, string marca, string modelo, int km, double precio, bool sidecar)
+            : base(id, marca, modelo, km, precio)
+        {
+            this.sidecar = sidecar;
+        }
+
+        public bool sidecar { get => _sidecar; set => _sidecar = value; }
+
+        public override double precio
+        {
+            get
+            {
+                if (this.sidecar)
+                {
+                    return base.precio + 50;
+                }
+                else
+                {
+                    return base.precio;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            if (this.sidecar)
+            {
+                return base.ToString() + " y tiene sidecar";
+            }
+            else
+            {
+                return base.ToString() + " y no tiene sidecar";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia4Ejerci4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Coche c = new Coche(1, "BMW", "1", 500, 10000, false);
+            Moto m = new Moto(2, "Toyota", "2", 100, 5000, true);
+            Bus a = new Bus(3, "MARCOPOLO", "3", 200, 8000, false);
+
+            Console.WriteLine("Precio coche: " + c.precio);
+            Console.WriteLine("Precio moto: " + m.precio);
+            Console.WriteLine("Precio Bus: " + a.precio);
+
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(m.ToString());
+            Console.WriteLine(a.ToString());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
